3d7ee7f50ffc649bb2bd8e7c0a9819ff2e23af3b
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-06-23 16:18:55 +0100

    task 86

---
+++ myScript.js
@@ -0,0 +1,5 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+document.write("Name:\tElom, Nnaemeka Joseph<br><br>");
+document.write("Height:\t1.75m<br><br>");
+document.write("Country:\tNigeria\n");
